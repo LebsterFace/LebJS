2e1fbd0aa9ee489cda7bed5583f2d901d6fa4708
LebsterFace <samsalinsky@gmail.com>
2021-06-06 11:43:08 +0100

    Function dumping

--- src/xyz/lebster/core/value/Function.java
+++ src/xyz/lebster/core/value/Function.java
@@ -1,9 +1,16 @@
 package xyz.lebster.core.value;
 
 import xyz.lebster.core.node.ScopeNode;
+import xyz.lebster.core.runtime.Interpreter;
 
 public class Function extends Value<ScopeNode> {
 	public Function(ScopeNode value) {
 		super(Type.Function, value);
+	}
+
+	@Override
+	public void dump(int indent) {
+		Interpreter.dumpIndent(indent);
+		System.out.println("FunctionObject");
 	}
 }

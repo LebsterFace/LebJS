062710873cb03e4a79aa77092ec2d37d38492f08
Lebsterface <samsalinsky@gmail.com>
2021-06-10 22:42:26 +0100

    String appending

--- src/xyz/lebster/core/node/BinaryExpression.java
+++ src/xyz/lebster/core/node/BinaryExpression.java
@@ -1,13 +1,14 @@
 package xyz.lebster.core.node;
 
+import xyz.lebster.core.exception.NotImplementedException;
 import xyz.lebster.core.runtime.Interpreter;
 import xyz.lebster.core.exception.LanguageException;
 import xyz.lebster.core.value.NumericLiteral;
+import xyz.lebster.core.value.StringLiteral;
 import xyz.lebster.core.value.Type;
 import xyz.lebster.core.value.Value;
 
 public class BinaryExpression extends Expression implements ASTNode {
-	// FIXME: Values which are not Number should work!
 	public final Expression left;
 	public final Expression right;
 	public final BinaryOp op;
@@ -31,18 +32,28 @@ public class BinaryExpression extends Expression implements ASTNode {
 	public Value<?> execute(Interpreter interpreter) throws LanguageException {
 		final Value<?> leftValue = left.execute(interpreter);
 		final Value<?> rightValue = right.execute(interpreter);
-		if (leftValue.type != Type.Number || rightValue.type != Type.Number) {
-			throw new LanguageException("Currently, only numbers can be added!");
+
+		if (leftValue.type == Type.Number && rightValue.type == Type.Number) {
+			final double lhs = ((NumericLiteral) leftValue).value;
+			final double rhs = ((NumericLiteral) rightValue).value;
+
+			return new NumericLiteral(switch (op) {
+				case Add -> lhs + rhs;
+				case Subtract -> lhs - rhs;
+				case Divide -> lhs / rhs;
+				case Multiply -> lhs * rhs;
+			});
+		} else if (op == BinaryOp.Add && leftValue.type == Type.String && rightValue.type == Type.String) {
+			final String lhs = ((StringLiteral) leftValue).value;
+			final String rhs = ((StringLiteral) rightValue).value;
+
+			return new StringLiteral(lhs + rhs);
+		} else {
+			throw new NotImplementedException("BinaryExpression (" +
+				leftValue.type + ", " +
+				op + ", " +
+				rightValue.type +
+			")");
 		}
-
-		double lhs = ((NumericLiteral) leftValue).value;
-		double rhs = ((NumericLiteral) rightValue).value;
-
-		return new NumericLiteral(switch (op) {
-			case Add -> lhs + rhs;
-			case Subtract -> lhs - rhs;
-			case Divide -> lhs / rhs;
-			case Multiply -> lhs * rhs;
-		});
 	}
 }

88817587ee71eb9ce6091413ebc76d575daca940
Lebsterface <samsalinsky@gmail.com>
2021-06-19 03:48:56 +0100

    Main: Disable printing of tokens

--- src/xyz/lebster/core/Main.java
+++ src/xyz/lebster/core/Main.java
@@ -88,10 +88,10 @@ public class Main {
 
 	public static void execProgram(String source, boolean showDebug) {
 		final Token[] tokens = new Lexer(source).tokenize();
-		System.out.println("------- TOKENS -------");
-		if (showDebug) {
-			for (Token token : tokens) System.out.println(token);
-		}
+//		if (showDebug) {
+//			System.out.println("------- TOKENS -------");
+//			for (Token token : tokens) System.out.println(token);
+//		}
 
 		execProgram(new Parser(tokens).parse(), showDebug);
 	}

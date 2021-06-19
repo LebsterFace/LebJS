2198112a7ee115679290b8644664ffbfcd8a2e3f
Lebsterface <samsalinsky@gmail.com>
2021-06-19 07:44:49 +0100

    VariableDeclarator: Rename 'name' to 'identifier'

--- src/xyz/lebster/core/node/VariableDeclarator.java
+++ src/xyz/lebster/core/node/VariableDeclarator.java
@@ -6,16 +6,16 @@ import xyz.lebster.core.value.Undefined;
 import xyz.lebster.core.value.Value;
 
 public class VariableDeclarator implements ASTNode {
-	public final Identifier name;
+	public final Identifier identifier;
 	public final Expression init;
 
-	public VariableDeclarator(Identifier name, Expression init) {
-		this.name = name;
+	public VariableDeclarator(Identifier identifier, Expression init) {
+		this.identifier = identifier;
 		this.init = init;
 	}
 
-	public VariableDeclarator(Identifier name) {
-		this.name = name;
+	public VariableDeclarator(Identifier identifier) {
+		this.identifier = identifier;
 		this.init = new Undefined();
 	}
 
@@ -23,13 +23,13 @@ public class VariableDeclarator implements ASTNode {
 	public void dump(int indent) {
 		Interpreter.dumpIndent(indent);
 		System.out.print("VariableDeclarator '");
-		System.out.print(name.value);
+		System.out.print(identifier.value);
 		System.out.println("':");
 		init.dump(indent + 1);
 	}
 
 	@Override
 	public Value<?> execute(Interpreter interpreter) throws LanguageException {
-		return interpreter.declareVariable(name, init.execute(interpreter));
+		return interpreter.declareVariable(identifier, init.execute(interpreter));
 	}
 }

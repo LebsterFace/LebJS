004065b7827065d3daf38a7e9003e3e5788a778b
Lebsterface <samsalinsky@gmail.com>
2021-06-20 21:14:50 +0100

    StringPrototype: Fix reverse() function

--- src/xyz/lebster/core/value/prototype/StringPrototype.java
+++ src/xyz/lebster/core/value/prototype/StringPrototype.java
@@ -1,16 +1,19 @@
 package xyz.lebster.core.value.prototype;
 
-import xyz.lebster.core.value.Dictionary;
-import xyz.lebster.core.value.NativeFunction;
-import xyz.lebster.core.value.StringLiteral;
+import xyz.lebster.core.value.*;
+import xyz.lebster.exception.NotImplemented;
 
 public class StringPrototype extends Dictionary {
 	public static final StringPrototype instance = new StringPrototype();
 
 	public StringPrototype() {
 		set("reverse", new NativeFunction((interpreter, arguments) -> {
-			final String value = interpreter.thisValue().toStringLiteral().value;
-			return new StringLiteral(new StringBuilder(value).reverse().toString());
+			final Value<?> value = interpreter.thisValue();
+			if (!(value instanceof final StringWrapper wrapper)) {
+				throw new NotImplemented("StringPrototype.reverse for non-StringWrapper Value");
+			}
+
+			return new StringLiteral(new StringBuilder(wrapper.value.value).reverse().toString());
 		}));
 	}
 }

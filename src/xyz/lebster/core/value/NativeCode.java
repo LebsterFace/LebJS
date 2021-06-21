a22990cd79b78df5fabe2437f04d58312aad0ac8
Lebsterface <samsalinsky@gmail.com>
2021-06-21 01:06:51 +0100

    NativeCode: Allow `execute` to throw exceptions

--- src/xyz/lebster/core/value/NativeCode.java
+++ src/xyz/lebster/core/value/NativeCode.java
@@ -1,8 +1,9 @@
 package xyz.lebster.core.value;
 
 import xyz.lebster.core.runtime.Interpreter;
+import xyz.lebster.exception.LanguageException;
 
 @FunctionalInterface
 public interface NativeCode {
-	Value<?> execute(Interpreter interpreter, Value<?>[] arguments);
+	Value<?> execute(Interpreter interpreter, Value<?>[] arguments) throws LanguageException;
 }

66badf599f17d99d3fd01a95e9b55ebf02a691cc
Lebsterface <samsalinsky@gmail.com>
2021-06-24 04:53:46 +0100

    CannotParse: Add `cannot be parsed as` overload

--- src/xyz/lebster/exception/CannotParse.java
+++ src/xyz/lebster/exception/CannotParse.java
@@ -1,7 +1,13 @@
 package xyz.lebster.exception;
 
+import xyz.lebster.parser.TokenType;
+
 public class CannotParse extends ParseException {
-	public CannotParse(String message) {
-		super(message + " cannot be parsed.");
+	public CannotParse(String what) {
+		super(what + " cannot be parsed.");
+	}
+
+	public CannotParse(TokenType type, String what) {
+		super(type + " cannot be parsed as " + what);
 	}
 }

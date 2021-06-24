d826a9545e70cbcf5c9e3164be023e10677b4c2f
Lebsterface <samsalinsky@gmail.com>
2021-06-24 04:53:24 +0100

    ScopeNode: Add `append` overload

--- src/xyz/lebster/core/node/ScopeNode.java
+++ src/xyz/lebster/core/node/ScopeNode.java
@@ -1,11 +1,15 @@
 package xyz.lebster.core.node;
 
 import java.util.ArrayList;
+import java.util.List;
 
 abstract public class ScopeNode implements ASTNode {
 	public final ArrayList<ASTNode> children = new ArrayList<>();
 
 	public void append(ASTNode node) {
-		this.children.add(node);
+		children.add(node);
+	}
+	public void append(List<ASTNode> nodes) {
+		children.addAll(nodes);
 	}
 }

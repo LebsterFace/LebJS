41c3c445341db8602a3cc977d852dcca2c047722
Lebsterface <samsalinsky@gmail.com>
2021-06-26 18:04:35 +0100

    Testing: Expand `if` test

--- tests/if.js
+++ tests/if.js
@@ -1,6 +1,37 @@
-let num = 10 * 2;
-if (num === 20) {
-    print("Correct!")
+let result = null;
+if (10 * 2 === 20) {
+    result = true;
 } else {
-    print("Incorrect!")
+    result = false;
 }
+
+expect(true, result);
+if (true) expect(0,0);
+else expect(false, true);
+
+let a = 0;
+if (a === 0) {
+    expect(0, a);
+} else if (a === 1) {
+   expect(1, a);
+} else {
+    expect(2, a);
+}
+
+a = a + 1
+if (a === 0) {
+    expect(0, a);
+} else if (a === 1) {
+    expect(1, a);
+} else {
+    expect(2, a);
+}
+
+a = a + 1
+ if (a === 0) {
+     expect(0, a);
+ } else if (a === 1) {
+     expect(1, a);
+ } else {
+     expect(2, a);
+ }

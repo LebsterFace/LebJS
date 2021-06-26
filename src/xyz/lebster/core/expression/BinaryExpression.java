ba29e81456404c6a5358ff1e7a00c85c1952809f
Lebsterface <samsalinsky@gmail.com>
2021-06-26 11:51:39 +0100

    BinaryExpression: Simplify logic

--- src/xyz/lebster/core/expression/BinaryExpression.java
+++ src/xyz/lebster/core/expression/BinaryExpression.java
@@ -7,7 +7,6 @@ import xyz.lebster.core.value.StringLiteral;
 import xyz.lebster.core.value.Type;
 import xyz.lebster.core.value.Value;
 import xyz.lebster.exception.LanguageException;
-import xyz.lebster.exception.NotImplemented;
 
 public final class BinaryExpression extends Expression implements ASTNode {
 	private final Expression left;
@@ -35,24 +34,22 @@ public final class BinaryExpression extends Expression implements ASTNode {
 	public Value<?> execute(Interpreter interpreter) throws LanguageException {
 		final Value<?> leftValue = left.execute(interpreter);
 		final Value<?> rightValue = right.execute(interpreter);
-
-		if (leftValue.type == Type.Number && rightValue.type == Type.Number) {
-			final double lhs = (double) leftValue.value;
-			final double rhs = (double) rightValue.value;
-
-			return new NumericLiteral(switch (op) {
-				case Add -> lhs + rhs;
-				case Subtract -> lhs - rhs;
-				case Divide -> lhs / rhs;
-				case Multiply -> lhs * rhs;
-			});
-		} else if (op == BinaryOp.Add && leftValue.type == Type.String && rightValue.type == Type.String) {
-			final String lhs = (String) leftValue.value;
-			final String rhs = (String) rightValue.value;
-
-			return new StringLiteral(lhs + rhs);
-		} else {
-			throw new NotImplemented("BinaryExpression (" + leftValue.type + ", " + op + ", " + rightValue.type + ")");
+//		https://tc39.es/ecma262/#sec-applystringornumericbinaryoperator
+//		FIXME: toPrimitive
+		if (op == BinaryOp.Add && (leftValue.type == Type.String || rightValue.type == Type.String)) {
+			final String lstr = leftValue.toStringLiteral().value;
+			final String rstr = rightValue.toStringLiteral().value;
+			return new StringLiteral(lstr + rstr);
 		}
+
+		final double lnum = leftValue.toNumericLiteral().value;
+		final double rnum = rightValue.toNumericLiteral().value;
+
+		return new NumericLiteral(switch (op) {
+			case Add -> lnum + rnum;
+			case Subtract -> lnum - rnum;
+			case Divide -> lnum / rnum;
+			case Multiply -> lnum * rnum;
+		});
 	}
 }

9bdb5151f13cdfee1108c90de242f1e995b92b0e
Lebsterface <samsalinsky@gmail.com>
2021-06-26 14:06:19 +0100

    AssignmentOp: Move `dump` to `AssignmentExpression`

--- src/xyz/lebster/core/expression/AssignmentOp.java
+++ src/xyz/lebster/core/expression/AssignmentOp.java
@@ -1,13 +1,5 @@
 package xyz.lebster.core.expression;
 
-import xyz.lebster.core.runtime.Interpreter;
-
 public enum AssignmentOp {
-	Equals;
-
-	public void dump(int indent) {
-		Interpreter.dumpIndent(indent);
-		System.out.print("AssignmentOp: ");
-		System.out.println(this);
-	}
+	Equals
 }

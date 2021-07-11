0f97f5c10c3c6d79df6887b0f44caca425dc116b
LebsterFace <samsalinsky@gmail.com>
2021-07-11 01:39:47 +0100

    Add `StringRepresentation`
    This class is used to construct a string approximation of an AST

---
+++ src/xyz/lebster/interpreter/StringRepresentation.java
@@ -0,0 +1,40 @@
+package xyz.lebster.interpreter;
+
+public class StringRepresentation {
+	private final StringBuilder builder = new StringBuilder();
+	private int currentIndent = 0;
+
+	public void append(Object o) {
+		builder.append(o);
+	}
+
+	public int getCurrentIndent() {
+		return currentIndent;
+	}
+
+	public void appendLine(Object o) {
+		append(o);
+		appendLine();
+	}
+
+	public void appendLine() {
+		builder.append('\n');
+	}
+
+	public void indent() {
+		currentIndent++;
+	}
+
+	public void unindent() {
+		currentIndent--;
+	}
+
+	public void appendIndent() {
+		builder.append("\t".repeat(currentIndent));
+	}
+
+	@Override
+	public String toString() {
+		return builder.toString();
+	}
+}

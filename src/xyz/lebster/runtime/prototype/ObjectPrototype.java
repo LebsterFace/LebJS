92e46e3ed160bf15feb5fe065027d285d6326751
LebsterFace <samsalinsky@gmail.com>
2021-07-14 07:32:57 +0100

    StringPrototype: Use private constructor

--- src/xyz/lebster/runtime/prototype/ObjectPrototype.java
+++ src/xyz/lebster/runtime/prototype/ObjectPrototype.java
@@ -4,9 +4,7 @@ import xyz.lebster.node.value.Dictionary;
 
 public class ObjectPrototype extends Dictionary {
 	public static final ObjectPrototype instance = new ObjectPrototype();
-
-	private ObjectPrototype() {
-	}
+	private ObjectPrototype() {}
 
 	@Override
 	public Dictionary getPrototype() {
--- src/xyz/lebster/runtime/prototype/StringPrototype.java
+++ src/xyz/lebster/runtime/prototype/StringPrototype.java
@@ -5,8 +5,7 @@ import xyz.lebster.node.value.*;
 
 public class StringPrototype extends Dictionary {
 	public static final StringPrototype instance = new StringPrototype();
-
-	public StringPrototype() {
+	private StringPrototype() {
 		set("reverse", new NativeFunction((interpreter, arguments) -> {
 			final Value<?> value = interpreter.thisValue();
 			if (!(value instanceof final StringWrapper wrapper)) {

f69bbe46cf9fb8fe90109b3799909a9de43e18c9
LebsterFace <samsalinsky@gmail.com>
2021-08-05 04:24:03 +0100

    Undefined: Implement `represent`

--- src/xyz/lebster/core/node/value/Undefined.java
+++ src/xyz/lebster/core/node/value/Undefined.java
@@ -3,6 +3,7 @@ package xyz.lebster.core.node.value;
 import xyz.lebster.core.Dumper;
 import xyz.lebster.core.interpreter.AbruptCompletion;
 import xyz.lebster.core.interpreter.Interpreter;
+import xyz.lebster.core.interpreter.StringRepresentation;
 import xyz.lebster.core.runtime.TypeError;
 
 public final class Undefined extends Primitive<Void> {
@@ -23,6 +24,11 @@ public final class Undefined extends Primitive<Void> {
 	@Override
 	public Dictionary toDictionary(Interpreter interpreter) throws AbruptCompletion {
 		throw AbruptCompletion.error(new TypeError("Cannot convert undefined or null to object"));
+	}
+
+	@Override
+	public void represent(StringRepresentation representation) {
+		representation.append("undefined");
 	}
 
 	@Override

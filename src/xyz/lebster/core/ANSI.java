f505427547eb94f1e4d2c31b969131ec4d9ca716
LebsterFace <samsalinsky@gmail.com>
2021-12-27 16:19:00 +0000

    Update ANSI strings to use aixterm bright variants

--- src/xyz/lebster/core/ANSI.java
+++ src/xyz/lebster/core/ANSI.java
@@ -10,14 +10,16 @@ public final class ANSI {
 	public static final String CYAN = "\u001b[36m";
 	public static final String WHITE = "\u001b[37m";
 	public static final String RESET = "\u001b[0m";
-	public static final String BRIGHT_BLACK = "\u001b[30;1m";
-	public static final String BRIGHT_RED = "\u001b[31;1m";
-	public static final String BRIGHT_GREEN = "\u001b[32;1m";
-	public static final String BRIGHT_YELLOW = "\u001b[33;1m";
-	public static final String BRIGHT_BLUE = "\u001b[34;1m";
-	public static final String BRIGHT_MAGENTA = "\u001b[35;1m";
-	public static final String BRIGHT_CYAN = "\u001b[36;1m";
-	public static final String BRIGHT_WHITE = "\u001b[37;1m";
+
+	public static final String BRIGHT_BLACK = "\u001b[90m";
+	public static final String BRIGHT_RED = "\u001b[91m";
+	public static final String BRIGHT_GREEN = "\u001b[92m";
+	public static final String BRIGHT_YELLOW = "\u001b[93m";
+	public static final String BRIGHT_BLUE = "\u001b[94m";
+	public static final String BRIGHT_MAGENTA = "\u001b[95m";
+	public static final String BRIGHT_CYAN = "\u001b[96m";
+	public static final String BRIGHT_WHITE = "\u001b[97m";
+
 	public static final String BACKGROUND_BLACK = "\u001b[40m";
 	public static final String BACKGROUND_RED = "\u001b[41m";
 	public static final String BACKGROUND_GREEN = "\u001b[42m";
@@ -26,14 +28,16 @@ public final class ANSI {
 	public static final String BACKGROUND_MAGENTA = "\u001b[45m";
 	public static final String BACKGROUND_CYAN = "\u001b[46m";
 	public static final String BACKGROUND_WHITE = "\u001b[47m";
-	public static final String BACKGROUND_BRIGHT_BLACK = "\u001b[40;1m";
-	public static final String BACKGROUND_BRIGHT_RED = "\u001b[41;1m";
-	public static final String BACKGROUND_BRIGHT_GREEN = "\u001b[42;1m";
-	public static final String BACKGROUND_BRIGHT_YELLOW = "\u001b[43;1m";
-	public static final String BACKGROUND_BRIGHT_BLUE = "\u001b[44;1m";
-	public static final String BACKGROUND_BRIGHT_MAGENTA = "\u001b[45;1m";
-	public static final String BACKGROUND_BRIGHT_CYAN = "\u001b[46;1m";
-	public static final String BACKGROUND_BRIGHT_WHITE = "\u001b[47;1m";
+
+	public static final String BACKGROUND_BRIGHT_BLACK = "\u001b[100m";
+	public static final String BACKGROUND_BRIGHT_RED = "\u001b[101m";
+	public static final String BACKGROUND_BRIGHT_GREEN = "\u001b[102m";
+	public static final String BACKGROUND_BRIGHT_YELLOW = "\u001b[103m";
+	public static final String BACKGROUND_BRIGHT_BLUE = "\u001b[104m";
+	public static final String BACKGROUND_BRIGHT_MAGENTA = "\u001b[105m";
+	public static final String BACKGROUND_BRIGHT_CYAN = "\u001b[106m";
+	public static final String BACKGROUND_BRIGHT_WHITE = "\u001b[107m";
+
 	public static final String BOLD = "\u001b[1m";
 	public static final String UNDERLINE = "\u001b[4m";
 	public static final String REVERSED = "\u001b[7m";

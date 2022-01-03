7428638b34484701219b1ae0201a78c98a52cbbe
LebsterFace <samsalinsky@gmail.com>
2022-01-03 20:57:21 +0000

    StringPrototype: Improve `reverse`

--- src/xyz/lebster/core/runtime/prototype/StringPrototype.java
+++ src/xyz/lebster/core/runtime/prototype/StringPrototype.java
@@ -1,22 +1,22 @@
 package xyz.lebster.core.runtime.prototype;
 
-import xyz.lebster.core.exception.NotImplemented;
+import xyz.lebster.core.NonStandard;
+import xyz.lebster.core.interpreter.AbruptCompletion;
+import xyz.lebster.core.interpreter.Interpreter;
 import xyz.lebster.core.node.value.Dictionary;
 import xyz.lebster.core.node.value.StringLiteral;
-import xyz.lebster.core.node.value.StringWrapper;
 import xyz.lebster.core.node.value.Value;
 
 public final class StringPrototype extends Dictionary {
 	public static final StringPrototype instance = new StringPrototype();
 
 	private StringPrototype() {
-		this.setMethod("reverse", (interpreter, arguments) -> {
-			final Value<?> value = interpreter.thisValue();
-			if (!(value instanceof final StringWrapper wrapper)) {
-				throw new NotImplemented("StringPrototype.reverse for non-StringWrapper Value");
-			}
+		this.setMethod("reverse", StringPrototype::reverse);
+	}
 
-			return new StringLiteral(new StringBuilder(wrapper.value.value).reverse().toString());
-		});
+	@NonStandard
+	private static StringLiteral reverse(Interpreter interpreter, Value<?>[] args) throws AbruptCompletion {
+		final String S = interpreter.thisValue().toStringLiteral(interpreter).value;
+		return new StringLiteral(new StringBuilder(S).reverse().toString());
 	}
 }

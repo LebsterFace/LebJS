d1bc0aa1a8212435745a0816c69390fe74854551
LebsterFace <samsalinsky@gmail.com>
2022-01-09 01:28:01 +0000

    ThisKeyword: Inline useless `StaticIdentifier` class

--- src/xyz/lebster/core/node/expression/ThisKeyword.java
+++ src/xyz/lebster/core/node/expression/ThisKeyword.java
@@ -1,16 +1,22 @@
 package xyz.lebster.core.node.expression;
 
+import xyz.lebster.core.Dumper;
 import xyz.lebster.core.interpreter.Interpreter;
+import xyz.lebster.core.interpreter.StringRepresentation;
 import xyz.lebster.core.node.value.Value;
 
-public final class ThisKeyword extends StaticIdentifier {
+public final class ThisKeyword implements Expression {
 	@Override
 	public Value<?> execute(Interpreter interpreter) {
 		return interpreter.thisValue();
 	}
 
+	public void dump(int indent) {
+		Dumper.dumpValue(indent, "Identifier", "this");
+	}
+
 	@Override
-	public String getName() {
-		return "this";
+	public void represent(StringRepresentation representation) {
+		representation.append("this");
 	}
 }

c33c40d147b006d411963508fe9869df3247086f
LebsterFace <samsalinsky@gmail.com>
2022-01-09 04:28:48 +0000

    Add `Names` class

---
+++ src/xyz/lebster/core/runtime/Names.java
@@ -0,0 +1,12 @@
+package xyz.lebster.core.runtime;
+
+import xyz.lebster.core.node.value.StringValue;
+
+public final class Names {
+	public static final StringValue push = new StringValue("push");
+	public static final StringValue map = new StringValue("map");
+	public static final StringValue join = new StringValue("join");
+	public static final StringValue toString = new StringValue("toString");
+	public static final StringValue valueOf = new StringValue("valueOf");
+	public static final StringValue toLocaleString = new StringValue("toLocaleString");
+}

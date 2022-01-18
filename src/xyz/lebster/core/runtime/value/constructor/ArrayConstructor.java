c87f8135d970ece1e062f959429edbfaa76d8d4e
LebsterFace <samsalinsky@gmail.com>
2022-01-18 21:38:45 +0000

    ArrayConstructor: Implement non-standard `of` method

--- src/xyz/lebster/core/runtime/value/constructor/ArrayConstructor.java
+++ src/xyz/lebster/core/runtime/value/constructor/ArrayConstructor.java
@@ -1,20 +1,47 @@
 package xyz.lebster.core.runtime.value.constructor;
 
+import xyz.lebster.core.NonStandard;
 import xyz.lebster.core.SpecificationURL;
 import xyz.lebster.core.exception.NotImplemented;
+import xyz.lebster.core.interpreter.AbruptCompletion;
 import xyz.lebster.core.interpreter.Interpreter;
+import xyz.lebster.core.interpreter.StringRepresentation;
 import xyz.lebster.core.runtime.value.Value;
+import xyz.lebster.core.runtime.value.error.TypeError;
+import xyz.lebster.core.runtime.value.executable.Executable;
 import xyz.lebster.core.runtime.value.object.ArrayObject;
+import xyz.lebster.core.runtime.value.primitive.NumberValue;
+import xyz.lebster.core.runtime.value.primitive.UndefinedValue;
 
 @SpecificationURL("https://tc39.es/ecma262/multipage#sec-array-constructor")
 public class ArrayConstructor extends BuiltinConstructor<ArrayObject> {
 	public static final ArrayConstructor instance = new ArrayConstructor();
 
 	static {
+		instance.setMethod("of", ArrayConstructor::of);
+
 	}
 
 	private ArrayConstructor() {
 		super();
+	}
+
+	@NonStandard
+	private static Value<?> of(Interpreter interpreter, Value<?>[] arguments) throws AbruptCompletion {
+		final int len = arguments.length > 0 ? arguments[0].toNumberValue(interpreter).value.intValue() : 0;
+		final Value<?> callbackFn = arguments.length > 1 ? arguments[1] : UndefinedValue.instance;
+		final Value<?> thisArg = arguments.length > 2 ? arguments[2] : UndefinedValue.instance;
+
+		if (!(callbackFn instanceof final Executable<?> executable)) {
+			final StringRepresentation builder = new StringRepresentation();
+			callbackFn.display(builder);
+			builder.append(" is not a function");
+			throw AbruptCompletion.error(new TypeError(builder.toString()));
+		}
+
+		final Value<?>[] result = new Value<?>[(int) len];
+		for (int k = 0; k < len; k++) result[k] = executable.call(interpreter, thisArg, new NumberValue(k));
+		return new ArrayObject(result);
 	}
 
 	@Override

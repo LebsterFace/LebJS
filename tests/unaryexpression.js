30375f2f2a52e589621fe8cba7f4086d59b1669c
LebsterFace <samsalinsky@gmail.com>
2022-01-18 20:09:27 +0000

    Tests: Un-indent `unaryexpression.js`

--- tests/unaryexpression.js
+++ tests/unaryexpression.js
@@ -3,9 +3,9 @@ expect(false, !true);
 expect(true, !false);
 expect(false, !!false);
 expect(true, !!!false);
- let n = 5
- expect(5, n)
- expect(4, --n)
- expect(5, ++n)
- expect(5, n--)
- expect(4, n++)
+let n = 5;
+expect(5, n);
+expect(4, --n);
+expect(5, ++n);
+expect(5, n--);
+expect(4, n++);

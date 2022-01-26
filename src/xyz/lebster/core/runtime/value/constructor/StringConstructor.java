476ab63dc9ab5da81fb9c53b64048a7a307a8297
LebsterFace <samsalinsky@gmail.com>
2022-01-26 05:30:49 +0000

    StringConstructor: Implement `call`

--- src/xyz/lebster/core/runtime/value/constructor/StringConstructor.java
+++ src/xyz/lebster/core/runtime/value/constructor/StringConstructor.java
@@ -2,10 +2,13 @@ package xyz.lebster.core.runtime.value.constructor;
 
 import xyz.lebster.core.SpecificationURL;
 import xyz.lebster.core.exception.NotImplemented;
+import xyz.lebster.core.interpreter.AbruptCompletion;
 import xyz.lebster.core.interpreter.Interpreter;
 import xyz.lebster.core.runtime.Names;
 import xyz.lebster.core.runtime.value.Value;
 import xyz.lebster.core.runtime.value.object.StringWrapper;
+import xyz.lebster.core.runtime.value.primitive.StringValue;
+import xyz.lebster.core.runtime.value.primitive.UndefinedValue;
 import xyz.lebster.core.runtime.value.prototype.StringPrototype;
 
 @SpecificationURL("https://tc39.es/ecma262/multipage#sec-string-constructor")
@@ -25,8 +28,9 @@ public class StringConstructor extends BuiltinConstructor<StringWrapper> {
 	}
 
 	@Override
-	public Value<?> call(Interpreter interpreter, Value<?>... arguments) {
-		throw new NotImplemented("String()");
+	public StringValue call(Interpreter interpreter, Value<?>... arguments) throws AbruptCompletion {
+		final Value<?> v = arguments.length == 0 ? UndefinedValue.instance : arguments[0];
+		return v.toStringValue(interpreter);
 	}
 
 	@Override

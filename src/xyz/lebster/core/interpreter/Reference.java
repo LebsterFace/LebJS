be53e2a2b0dfc155bfa71a2d39eea902fc3fa149
LebsterFace <samsalinsky@gmail.com>
2022-01-31 01:09:10 +0000

    Reference: Remove FIXME

--- src/xyz/lebster/core/interpreter/Reference.java
+++ src/xyz/lebster/core/interpreter/Reference.java
@@ -7,10 +7,6 @@ import xyz.lebster.core.runtime.value.object.ObjectValue;
 import xyz.lebster.core.runtime.value.primitive.StringValue;
 
 @SpecificationURL("https://tc39.es/ecma262/multipage#sec-reference-record-specification-type")
-// FIXME: The `base` 'should' be possible to be a LexicalEnvironment as well as an ObjectLiteral
-// 		  Right now, when we need to use a LexicalEnvironment we just pass in the variables
-//		  of the frame (Interpreter#getReference resolvable case), but in the future
-//		  we may need to pass the entire LexicalEnvironment
 public record Reference(ObjectValue base, StringValue referencedName) {
 	@SpecificationURL("https://tc39.es/ecma262/multipage#sec-getvalue")
 	public Value<?> getValue(Interpreter interpreter) throws AbruptCompletion {

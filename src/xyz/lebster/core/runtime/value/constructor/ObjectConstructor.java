a1b74f1af8386065049e09f31b62b684a1821f64
LebsterFace <samsalinsky@gmail.com>
2022-02-01 03:00:00 +0000

    ObjectConstructor: Extract to method references

--- src/xyz/lebster/core/runtime/value/constructor/ObjectConstructor.java
+++ src/xyz/lebster/core/runtime/value/constructor/ObjectConstructor.java
@@ -19,43 +19,47 @@ public final class ObjectConstructor extends BuiltinConstructor<ObjectValue> {
 	static {
 		instance.putNonWritable(Names.prototype, ObjectPrototype.instance);
 
-		instance.putMethod("setPrototypeOf", (interpreter, arguments) -> {
-			final Value<?> O = arguments.length > 0 ? arguments[0] : UndefinedValue.instance;
-			final Value<?> proto = arguments.length > 1 ? arguments[1] : UndefinedValue.instance;
-
-			// 1. Set O to ? RequireObjectCoercible(O).
-			if (O.isNullish())
-				throw AbruptCompletion.error(new TypeError("Object.setPrototypeOf called on null or undefined"));
-
-			// 2. If Type(proto) is neither Object nor Null, throw a TypeError exception.
-			ObjectValue proto_objectValue = null;
-			if (proto instanceof final ObjectValue p) {
-				proto_objectValue = p;
-			} else if (proto != NullValue.instance) {
-				throw AbruptCompletion.error(new TypeError("Object prototype may only be an Object or null"));
-			}
-
-			// 3. If Type(O) is not Object, return O.
-			if (!(O instanceof final ObjectValue O_objectValue)) return O;
-			// 4. Let status be ? O.[[SetPrototypeOf]](proto).
-			final boolean status = O_objectValue.setPrototype(proto_objectValue);
-			// 5. If status is false, throw a TypeError exception.
-			// 6. Return O.
-
-			return UndefinedValue.instance;
-		});
-
-		instance.putMethod("getPrototypeOf", (interpreter, arguments) -> {
-			final Value<?> O = arguments.length > 0 ? arguments[0] : UndefinedValue.instance;
-			// 1. Let obj be ? ToObject(O).
-			// 2. Return ? obj.[[GetPrototypeOf]]().
-			final ObjectValue prototype = O.toObjectValue(interpreter).getPrototype();
-			return prototype == null ? NullValue.instance : prototype;
-		});
+		instance.putMethod("setPrototypeOf", ObjectConstructor::setPrototypeOf);
+		instance.putMethod("getPrototypeOf", ObjectConstructor::getPrototypeOf);
 	}
 
 	private ObjectConstructor() {
 		super();
+	}
+
+	private static Value<?> setPrototypeOf(Interpreter interpreter, Value<?>[] arguments) throws AbruptCompletion {
+		final Value<?> O = arguments.length > 0 ? arguments[0] : UndefinedValue.instance;
+		final Value<?> proto = arguments.length > 1 ? arguments[1] : UndefinedValue.instance;
+
+		// 1. Set O to ? RequireObjectCoercible(O).
+		if (O.isNullish())
+			throw AbruptCompletion.error(new TypeError("Object.setPrototypeOf called on null or undefined"));
+
+		// 2. If Type(proto) is neither Object nor Null, throw a TypeError exception.
+		ObjectValue proto_objectValue = null;
+		if (proto instanceof final ObjectValue p) {
+			proto_objectValue = p;
+		} else if (proto != NullValue.instance) {
+			throw AbruptCompletion.error(new TypeError("Object prototype may only be an Object or null"));
+		}
+
+		// 3. If Type(O) is not Object, return O.
+		if (!(O instanceof final ObjectValue O_objectValue)) return O;
+		// 4. Let status be ? O.[[SetPrototypeOf]](proto).
+		final boolean status = O_objectValue.setPrototype(proto_objectValue);
+		// 5. If status is false, throw a TypeError exception.
+		if (!status)
+			throw AbruptCompletion.error(new TypeError("Cyclic object prototype value"));
+		// 6. Return O.
+		return O;
+	}
+
+	private static Value<?> getPrototypeOf(Interpreter interpreter, Value<?>[] arguments) throws AbruptCompletion {
+		final Value<?> O = arguments.length > 0 ? arguments[0] : UndefinedValue.instance;
+		// 1. Let obj be ? ToObject(O).
+		// 2. Return ? obj.[[GetPrototypeOf]]().
+		final ObjectValue prototype = O.toObjectValue(interpreter).getPrototype();
+		return prototype == null ? NullValue.instance : prototype;
 	}
 
 	@Override

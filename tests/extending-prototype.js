4782f5167b3364ae093d4207c25d3379aa6ec66d
LebsterFace <samsalinsky@gmail.com>
2022-02-02 09:01:01 +0000

    Tests: Add more to `extending-prototypes.js`

--- tests/extending-prototype.js
+++ tests/extending-prototype.js
@@ -10,3 +10,12 @@ expect(true, String.prototype.hasOwnProperty("firstCharacter"));
 expect(true, myString.hasProperty("firstCharacter"));
 expect(String.prototype.firstCharacter, myString.firstCharacter);
 expect("H", myString.firstCharacter());
+
+String.prototype.append = function(value) {
+	return this + value;
+};
+
+expect("hello", "hell".append("o"));
+
+Number.prototype.multiply = (x) => x * this;
+expect(15, (3).multiply(5));

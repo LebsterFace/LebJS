9033c957791ef4ffa2308e4b453d4df511f2a5a3
LebsterFace <samsalinsky@gmail.com>
2022-02-07 01:16:39 +0000

    ShadowRealmPrototype: Set correct value for `constructor`

--- src/xyz/lebster/core/runtime/value/prototype/ShadowRealmPrototype.java
+++ src/xyz/lebster/core/runtime/value/prototype/ShadowRealmPrototype.java
@@ -1,12 +1,13 @@
 package xyz.lebster.core.runtime.value.prototype;
 
+import xyz.lebster.core.runtime.value.constructor.ShadowRealmConstructor;
 import xyz.lebster.core.runtime.value.object.ObjectValue;
 
 public final class ShadowRealmPrototype extends ObjectValue {
 	public static final ShadowRealmPrototype instance = new ShadowRealmPrototype();
 
 	static {
-		instance.put("constructor", ShadowRealmPrototype.instance);
+		instance.put("constructor", ShadowRealmConstructor.instance);
 	}
 
 	private ShadowRealmPrototype() {

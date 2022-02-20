f8f68d52c1a689883200022c5ff52aff951dfda1
LebsterFace <samsalinsky@gmail.com>
2022-02-20 18:47:03 +0000

    Tests: Update `array.js`

--- tests/array.js
+++ tests/array.js
@@ -11,16 +11,25 @@ expect(4, arr[3])
 expect(4, arr["3"])
 expect(false, arr.hasOwnProperty("4"))
 
+expect(false, arr.hasOwnProperty("prop"))
+expect(undefined, arr["prop"]);
+expect(undefined, arr.prop);
 arr.prop = 200;
-expect(200, arr.prop)
+expect(true, arr.hasOwnProperty("prop"))
+expect(200, arr["prop"]);
+expect(200, arr.prop);
 
 arr.push(5);
 expect(5, arr.length);
 expect(5, arr[4])
 expect(5, arr[arr.length - 1])
+arr.push(6, 7, 8, 9);
+expect(9, arr.length);
+expect(6, arr["5"])
+expect(9, arr[arr.length - 1])
 
-expect("1,2,3,4,5", arr.join())
-expect("1|2|3|4|5", arr.join("|"))
+expect("1,2,3,4,5,6,7,8,9", arr.join())
+expect("1|2|3|4|5|6|7|8|9", arr.join("|"))
 
-expect("2,4,6,8,10", arr.map(n => n * 2).toString())
-expect("2,4", arr.filter(n => n % 2 === 0).toString())
+expect("2,4,6,8,10,12,14,16,18", arr.map(n => n * 2).toString())
+expect("2,4,6,8", arr.filter(n => n % 2 === 0).toString())

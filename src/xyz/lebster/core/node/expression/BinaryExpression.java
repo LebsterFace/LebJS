02d6444b0738a42afe1a0b9c815e0b6eb43b9aa1
LebsterFace <samsalinsky@gmail.com>
2022-02-27 13:18:16 +0000

    BinaryExpression: Implement `<<`, `>>`, `>>>`, `&`, `|`, `^`

--- src/xyz/lebster/core/node/expression/BinaryExpression.java
+++ src/xyz/lebster/core/node/expression/BinaryExpression.java
@@ -2,13 +2,14 @@ package xyz.lebster.core.node.expression;
 
 import xyz.lebster.core.Dumper;
 import xyz.lebster.core.SpecificationURL;
-import xyz.lebster.core.exception.NotImplemented;
 import xyz.lebster.core.interpreter.AbruptCompletion;
 import xyz.lebster.core.interpreter.Interpreter;
 import xyz.lebster.core.interpreter.StringRepresentation;
 import xyz.lebster.core.runtime.value.Value;
 import xyz.lebster.core.runtime.value.primitive.NumberValue;
 import xyz.lebster.core.runtime.value.primitive.StringValue;
+
+import static xyz.lebster.core.runtime.value.primitive.NumberValue.UINT32_LIMIT;
 
 public record BinaryExpression(Expression left, Expression right, BinaryOp op) implements Expression {
 	@SpecificationURL("https://tc39.es/ecma262/multipage#sec-applystringornumericbinaryoperator")
@@ -26,17 +27,29 @@ public record BinaryExpression(Expression left, Expression right, BinaryOp op) i
 			}
 		}
 
-		final double left_num = left_value.toNumberValue(interpreter).value;
-		final double right_num = right_value.toNumberValue(interpreter).value;
+		final NumberValue left_number = left_value.toNumberValue(interpreter);
+		final NumberValue right_number = right_value.toNumberValue(interpreter);
 
 		return new NumberValue(switch (op) {
-			case Add -> left_num + right_num;
-			case Subtract -> left_num - right_num;
-			case Multiply -> left_num * right_num;
-			case Divide -> left_num / right_num;
-			case Exponentiate -> Math.pow(left_num, right_num);
-			case Remainder -> left_num % right_num;
-			default -> throw new NotImplemented(op.name());
+			case Add -> left_number.value + right_number.value;
+			case Subtract -> left_number.value - right_number.value;
+			case Multiply -> left_number.value * right_number.value;
+			case Divide -> left_number.value / right_number.value;
+			case Exponentiate -> Math.pow(left_number.value, right_number.value);
+			case Remainder -> left_number.value % right_number.value;
+
+			// https://tc39.es/ecma262/multipage#sec-numeric-types-number-leftShift
+			case LeftShift -> (double) (left_number.toInt32() << (right_number.toUint32() % 32));
+			case SignedRightShift -> (double) (left_number.toInt32() >> (right_number.toUint32() % 32));
+			case UnsignedRightShift -> {
+				final long result = (int) left_number.toUint32() >>> ((int) right_number.toUint32()) % 32;
+				yield (double) (result & UINT32_LIMIT);
+			}
+
+			// https://tc39.es/ecma262/multipage#sec-numberbitwiseop
+			case BitwiseAND -> (double) (left_number.toInt32() & right_number.toInt32());
+			case BitwiseXOR -> (double) (left_number.toInt32() ^ right_number.toInt32());
+			case BitwiseOR -> (double) (left_number.toInt32() | right_number.toInt32());
 		});
 	}
 
@@ -80,6 +93,7 @@ public record BinaryExpression(Expression left, Expression right, BinaryOp op) i
 		BitwiseOR("|");
 
 		private final String str;
+
 		BinaryOp(String str) {
 			this.str = str;
 		}

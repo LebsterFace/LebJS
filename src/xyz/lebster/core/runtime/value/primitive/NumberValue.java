03dbb91675ad1df60aaeb51cb4d6a34e687b8e98
LebsterFace <samsalinsky@gmail.com>
2022-02-27 10:57:23 +0000

    NumberValue#stringValueOf: `-0` should become `'0'`

--- src/xyz/lebster/core/runtime/value/primitive/NumberValue.java
+++ src/xyz/lebster/core/runtime/value/primitive/NumberValue.java
@@ -53,8 +53,8 @@ public final class NumberValue extends PrimitiveValue<Double> {
 	@SpecificationURL("https://tc39.es/ecma262/multipage#sec-numeric-types-number-tostring")
 	@NonCompliant
 	private static String stringValueOf(Double d) {
+		if (d == 0.0) return "0";
 		if (d.isNaN()) return "NaN";
-		else if (isPositiveZero(d)) return "0";
 		else if (d < 0.0) return "-" + stringValueOf(-d);
 		else if (d.isInfinite()) return "Infinity";
 		final String input = String.valueOf(d);

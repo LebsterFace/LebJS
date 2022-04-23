dff2030fd4789c778d6532e9680696bbfb382f76
LebsterFace <samsalinsky@gmail.com>
2022-04-23 23:24:55 +0100

    StringWrapper: Support character indexing

--- src/xyz/lebster/core/runtime/value/object/StringWrapper.java
+++ src/xyz/lebster/core/runtime/value/object/StringWrapper.java
@@ -1,17 +1,85 @@
 package xyz.lebster.core.runtime.value.object;
 
 import xyz.lebster.core.SpecificationURL;
+import xyz.lebster.core.exception.ShouldNotHappen;
 import xyz.lebster.core.runtime.Names;
 import xyz.lebster.core.runtime.value.HasBuiltinTag;
+import xyz.lebster.core.runtime.value.object.property.DataDescriptor;
+import xyz.lebster.core.runtime.value.object.property.PropertyDescriptor;
 import xyz.lebster.core.runtime.value.primitive.NumberValue;
 import xyz.lebster.core.runtime.value.primitive.StringValue;
 import xyz.lebster.core.runtime.value.prototype.StringPrototype;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.PrimitiveIterator;
 
 @SpecificationURL("https://tc39.es/ecma262/multipage#sec-string-objects")
 public final class StringWrapper extends PrimitiveWrapper<StringValue> implements HasBuiltinTag {
 	public StringWrapper(StringValue s) {
 		super(s);
 		this.put(Names.length, new NumberValue(s.value.length()));
+	}
+
+
+	@Override
+	public PropertyDescriptor getOwnProperty(Key<?> key) {
+		final PropertyDescriptor fromMap = this.value.get(key);
+		if (fromMap != null) return fromMap;
+
+		final int index = key.toIndex();
+		if (index == -1 || index >= this.data.value.length()) return null;
+		final var character = new StringValue(this.data.value.charAt(index));
+		return new DataDescriptor(character, false, true, false);
+	}
+
+	@Override
+	public boolean hasOwnProperty(Key<?> key) {
+		if (this.value.containsKey(key)) return true;
+		final int index = key.toIndex();
+		return index != -1 && index < this.data.value.length();
+	}
+
+	@Override
+	public Iterable<Map.Entry<Key<?>, PropertyDescriptor>> entries() {
+		return () -> new Iterator<>() {
+			private final PrimitiveIterator.OfInt primitiveIterator = data.value.chars().iterator();
+			private final Iterator<Map.Entry<Key<?>, PropertyDescriptor>> x = StringWrapper.super.entries().iterator();
+			private int charIndex = 0;
+
+			@Override
+			public boolean hasNext() {
+				return primitiveIterator.hasNext() || x.hasNext();
+			}
+
+			@Override
+			public Map.Entry<Key<?>, PropertyDescriptor> next() {
+				if (primitiveIterator.hasNext()) {
+					final var key = new StringValue(charIndex);
+					final var value = new DataDescriptor(new StringValue((char) (int) primitiveIterator.next()), false, true, false);
+					charIndex++;
+
+					return new Map.Entry<>() {
+						@Override
+						public Key<?> getKey() {
+							return key;
+						}
+
+						@Override
+						public PropertyDescriptor getValue() {
+							return value;
+						}
+
+						@Override
+						public PropertyDescriptor setValue(PropertyDescriptor value) {
+							throw new ShouldNotHappen("Strings are immutable");
+						}
+					};
+				}
+
+				return x.next();
+			}
+		};
 	}
 
 	@Override

ea38019b18be5457e60f0305c54c12bc50d9f156
LebsterFace <samsalinsky@gmail.com>
2022-04-29 16:49:47 +0100

    TokenType: Format enum constants

--- src/xyz/lebster/core/parser/TokenType.java
+++ src/xyz/lebster/core/parser/TokenType.java
@@ -1,14 +1,16 @@
 package xyz.lebster.core.parser;
 
 public enum TokenType {
-	Bang, Break, Case, Catch, Class, Comma, Const, Continue, Debugger, Default, Delete, Slash, Do, EOF,
-	Else, Equals, Export, Extends, Finally, For, Function, Identifier, If, Import, In, Instanceof, LBrace, LBracket,
-	LParen, Let, Minus, Star, New, NumericLiteral, Period, Plus, RBrace, RBracket, RParen, Return, Semicolon,
-	StringLiteral, Super, Switch, LineTerminator, This, Throw, Try, Typeof, Var, Void, While, With, Yield, Null, Undefined,
-	NaN, UnsignedRightShiftEquals, StrictEqual, LeftShiftEquals, Ampersand, Percent, Caret, LessThan, GreaterThan, Pipe,
-	Tilde, MinusMinus, MinusEquals, NotEqual, NullishCoalescing, OptionalChain, Exponent, MultiplyEquals, DivideEquals,
-	LogicalAnd, AmpersandEquals, PercentEquals, CaretEquals, PlusPlus, PlusEquals, LeftShift, LessThanEqual, LooseEqual,
-	Arrow, GreaterThanEqual, RightShift, PipeEquals, LogicalOr, StrictNotEqual, NullishCoalescingEquals, ExponentEquals,
-	LogicalAndEquals, RightShiftEquals, UnsignedRightShift, LogicalOrEquals, Infinity, QuestionMark, Await, Colon,
-	Backslash, DotDotDot, Enum, False, At, Hashtag, True
+	Ampersand, AmpersandEquals, Arrow, At, Await, Backslash, Bang, Break, Caret, CaretEquals,
+	Case, Catch, Class, Colon, Comma, Const, Continue, Debugger, Default, Delete, DivideEquals,
+	Do, DotDotDot, EOF, Else, Enum, Equals, Exponent, ExponentEquals, Export, Extends, False,
+	Finally, For, Function, GreaterThan, GreaterThanEqual, Hashtag, Identifier, If, Import, In,
+	Infinity, Instanceof, LBrace, LBracket, LParen, LeftShift, LeftShiftEquals, LessThan,
+	LessThanEqual, Let, LineTerminator, LogicalAnd, LogicalAndEquals, LogicalOr, LogicalOrEquals,
+	LooseEqual, Minus, MinusEquals, MinusMinus, MultiplyEquals, NaN, New, NotEqual, Null,
+	NullishCoalescing, NullishCoalescingEquals, NumericLiteral, OptionalChain, Percent,
+	PercentEquals, Period, Pipe, PipeEquals, Plus, PlusEquals, PlusPlus, QuestionMark, RBrace,
+	RBracket, RParen, Return, RightShift, RightShiftEquals, Semicolon, Slash, Star, StrictEqual,
+	StrictNotEqual, StringLiteral, Super, Switch, This, Throw, Tilde, True, Try, Typeof, Undefined,
+	UnsignedRightShift, UnsignedRightShiftEquals, Var, Void, While, With, Yield
 }

3a4d211a3269650ea7d3595c9af6d03231c7e561
LebsterFace <samsalinsky@gmail.com>
2022-08-09 02:59:28 +0100

    StringConstructor: Implement `String(<symbol>)`

--- src/xyz/lebster/core/value/string/StringConstructor.java
+++ src/xyz/lebster/core/value/string/StringConstructor.java
@@ -9,6 +9,7 @@ import xyz.lebster.core.value.Value;
 import xyz.lebster.core.value.function.FunctionPrototype;
 import xyz.lebster.core.value.object.ObjectPrototype;
 import xyz.lebster.core.value.object.ObjectValue;
+import xyz.lebster.core.value.symbol.SymbolValue;
 
 @SpecificationURL("https://tc39.es/ecma262/multipage#sec-string-constructor")
 public class StringConstructor extends BuiltinConstructor<StringWrapper, StringPrototype> {
@@ -16,6 +17,7 @@ public class StringConstructor extends BuiltinConstructor<StringWrapper, StringP
 		super(objectPrototype, functionPrototype, Names.String);
 	}
 
+	@Override
 	public StringWrapper construct(Interpreter interpreter, Value<?>[] arguments, ObjectValue newTarget) throws AbruptCompletion {
 		final StringValue data = arguments.length == 0 ? StringValue.EMPTY : arguments[0].toStringValue(interpreter);
 		return new StringWrapper(interpreter.intrinsics.stringPrototype, data);
@@ -24,6 +26,7 @@ public class StringConstructor extends BuiltinConstructor<StringWrapper, StringP
 	@Override
 	public StringValue call(Interpreter interpreter, Value<?>... arguments) throws AbruptCompletion {
 		if (arguments.length == 0) return StringValue.EMPTY;
-		return arguments[0].toStringValue(interpreter);
+		else if (arguments[0] instanceof SymbolValue symbolValue) return new StringValue(symbolValue.toDisplayString());
+		else return arguments[0].toStringValue(interpreter);
 	}
 }

9822bd485bf754d43fb1ebb864417e073c557787
LebsterFace <samsalinsky@gmail.com>
2023-07-24 23:21:23 +0100

    Literal: Represent without ANSI formatting

--- src/xyz/lebster/core/node/expression/literal/BooleanLiteral.java
+++ src/xyz/lebster/core/node/expression/literal/BooleanLiteral.java
@@ -6,6 +6,6 @@ import xyz.lebster.core.value.primitive.boolean_.BooleanValue;
 public record BooleanLiteral(BooleanValue value) implements Literal<BooleanValue> {
 	@Override
 	public void represent(StringRepresentation representation) {
-		representation.append(value == BooleanValue.TRUE ? "true" : "false");
+		representation.append(value.value);
 	}
 }
--- src/xyz/lebster/core/node/expression/literal/StringLiteral.java
+++ src/xyz/lebster/core/node/expression/literal/StringLiteral.java
@@ -1,11 +1,12 @@
 package xyz.lebster.core.node.expression.literal;
 
+import xyz.lebster.core.StringEscapeUtils;
 import xyz.lebster.core.interpreter.StringRepresentation;
 import xyz.lebster.core.value.primitive.string.StringValue;
 
 public record StringLiteral(StringValue value) implements Literal<StringValue> {
 	@Override
 	public void represent(StringRepresentation representation) {
-		value.display(representation);
+		representation.append(StringEscapeUtils.quote(value.value, false));
 	}
 }
